212e8d630fb264a138fadfbfc2eaa61c72a0c21a
leviferre1ra <leviferreira1677@gmail.com>
2025-10-20 17:44:40 -0300

    Telas iniciais

--- crudRedis/crudRedis/settings.py
+++ crudRedis/crudRedis/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'crudRedis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
---
+++ crudRedis/crudRedis/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def list(request):
+    return render(request, 'list.html')
+
+def add(request):
+    return render(request, 'add.html')
